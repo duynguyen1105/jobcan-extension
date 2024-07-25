a99fffd4cc87411093b64ae25cad403141aef69d
nguyen.nd <nguyen.nd@josys.com>
2024-07-25 10:57:06 +0700

    github urls

--- src/contentscript.ts
+++ src/contentscript.ts
@@ -1,49 +1,36 @@
 chrome.runtime.onMessage.addListener((msg, sender, callback) => {
-  const { startTimeValue, endTimeValue, breakTimeValue, isAutoSendRequest } =
-    msg
-  const listDays = document.getElementsByClassName('align-middle border-left-0')
-  for (let index = 0; index < listDays.length; index++) {
-    const weekday = listDays[index] as HTMLElement
-    const parentElement = weekday.parentElement as HTMLElement
+  const { branch } = msg
+  console.log(branch, msg)
 
-    if (weekday.innerText.includes('T7') || weekday.innerText.includes('CN'))
-      continue
+  const listUrls = Array.from(
+    document.getElementsByClassName('issue-link js-issue-link')
+  )
+    .filter(
+      (i: any) =>
+        !i.nextElementSibling.innerHTML.includes(`josys-src/${branch}`) &&
+        !i.href.includes('axios')
+    )
+    .map((i: any) => `- ${i.href}`)
 
-    const startTime = parentElement.querySelector(
-      '#editable_start'
-    ) as HTMLElement
-    const endTime = parentElement.querySelector('#editable_end') as HTMLElement
-    const breakTime = parentElement.querySelector(
-      '#editable_rest'
-    ) as HTMLElement
+  const content = '\n' + listUrls.join('\n')
 
-    const startInput = startTime.childNodes[0] as HTMLInputElement
-    const endTimeInput = endTime.childNodes[0] as HTMLInputElement
-    const breakTimeInput = breakTime.childNodes[0] as HTMLInputElement
+  const actionBtn = document.getElementsByClassName(
+    'timeline-comment-action Link--secondary Button--link Button--medium Button'
+  )[0] as HTMLElement
 
-    if (!startInput.value) {
-      startInput.style.backgroundColor = 'aqua'
-      startInput.value = startTimeValue
-    }
+  actionBtn.click()
 
-    if (!endTimeInput.value) {
-      endTimeInput.style.backgroundColor = 'aqua'
-      endTimeInput.value = endTimeValue
-    }
+  setTimeout(() => {
+    const editBtn = document.getElementsByClassName(
+      'dropdown-item btn-link js-comment-edit-button'
+    )[0] as HTMLElement
+    editBtn.click()
+  }, 1000)
 
-    if (!breakTimeInput.value) {
-      breakTimeInput.style.backgroundColor = 'aqua'
-      breakTimeInput.value = breakTimeValue
-    }
-
-    if (
-      isAutoSendRequest &&
-      (!startInput.value || !endTimeInput.value || !breakTimeInput.value)
-    ) {
-      const button = parentElement.getElementsByClassName(
-        'btn jbc-btn-secondary'
-      )[0] as HTMLElement
-      button.click()
-    }
-  }
+  setTimeout(() => {
+    const description = document.getElementsByClassName(
+      'js-comment-field js-paste-markdown js-task-list-field js-quick-submit js-size-to-fit js-session-resumable CommentBox-input FormControl-textarea js-saved-reply-shortcut-comment-field'
+    )[0] as HTMLTextAreaElement
+    description.value += content
+  }, 1000)
 })
--- src/popup-page/popup.tsx
+++ src/popup-page/popup.tsx
@@ -2,19 +2,13 @@ import React, { useState } from 'react'
 import { render } from 'react-dom'
 
 export const Popup = () => {
-  const [startTimeValue, setStartTimeValue] = useState('09:00')
-  const [endTimeValue, setEndTimeValue] = useState('18:00')
-  const [breakTimeValue, setBreakTimeValue] = useState('01:00')
-  const [isAutoSendRequest, setIsAutoSendRequest] = useState(false)
+  const [branch, setBranch] = useState('main')
 
   const onSubmitForm = () => {
     chrome.tabs.query({ currentWindow: true, active: true }, (tabs) => {
       const currentTabID = tabs.length === 0 ? 0 : tabs[0].id!
       chrome.tabs.sendMessage(currentTabID, {
-        startTimeValue,
-        endTimeValue,
-        breakTimeValue,
-        isAutoSendRequest,
+        branch,
       })
     })
   }
@@ -33,61 +27,12 @@ export const Popup = () => {
         gap: '1rem',
       }}
     >
-      <div style={inputStyle}>
-        <label htmlFor="start-time">Start Time</label>
-        <input
-          id="start-time"
-          type="time"
-          value={startTimeValue}
-          onChange={(e) => setStartTimeValue(e.target.value)}
-          style={{
-            width: '50%',
-          }}
-        />
-      </div>
-      <div style={inputStyle}>
-        <label htmlFor="end-time">End Time</label>
-        <input
-          id="end-time"
-          type="time"
-          value={endTimeValue}
-          onChange={(e) => setEndTimeValue(e.target.value)}
-          style={{
-            width: '50%',
-          }}
-        />
-      </div>
-      <div style={inputStyle}>
-        <label htmlFor="break-time">Break Time</label>
-        <input
-          id="break-time"
-          type="time"
-          value={breakTimeValue}
-          onChange={(e) => setBreakTimeValue(e.target.value)}
-          style={{
-            width: '50%',
-          }}
-        />
-      </div>
-      <div
+      <label htmlFor="branches">Ignore PR sync from:</label>
+      <select
+        name="branches"
+        id="branches"
+        onChange={(v) => setBranch(v.target.value)}
         style={{
-          display: 'flex',
-          alignItems: 'center',
-          gap: '1rem',
-        }}
-      >
-        <input
-          type="checkbox"
-          id="auto-click-send-request"
-          checked={isAutoSendRequest}
-          onChange={(e) => setIsAutoSendRequest(e.target.checked)}
-        />
-        <label htmlFor="auto-click-send-request">Auto click send request</label>
-      </div>
-      <button
-        type="submit"
-        style={{
-          cursor: 'pointer',
           outline: 0,
           display: 'inline-block',
           fontWeight: 400,
@@ -104,7 +49,32 @@ export const Popup = () => {
           borderColor: '#0d6efd',
         }}
       >
-        Triểnn
+        <option value="main">main</option>
+        <option value="staging">staging</option>
+        <option value="production">production</option>
+      </select>
+      <button
+        type="submit"
+        style={{
+          cursor: 'pointer',
+          outline: 0,
+          display: 'inline-block',
+          fontWeight: 400,
+          lineHeight: 1.5,
+          textAlign: 'center',
+          backgroundColor: 'transparent',
+          border: '1px solid transparent',
+          padding: '6px 12px',
+          fontSize: '1rem',
+          borderRadius: '.25rem',
+          transition:
+            'color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out',
+          color: 'white',
+          background: '#0d6efd',
+          borderColor: '#0d6efd',
+        }}
+      >
+        Get URLs
       </button>
     </form>
   )
